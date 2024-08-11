907ab04c361c87eb52798ee84fc230c9c94ad625
AyoriaChagua <ayoriachagua@gmail.com>
2024-08-10 22:53:38 -0500

    fix: add interface to vite config d ts 2

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
 declare namespace NodeJS {
     interface ProcessEnv {
-        REACT_APP_SUPABASE_URL: string;
-        REACT_APP_SUPABASE_KEY: string;
+        VITE_SUPABASE_URL: string;
+        VITE_SUPABASE_KEY: string;
+        VITE_EMAIL_SERVICE_ID: string,
+        VITE_EMAIL_TEMPLATE_ID: string,
+        VITE_EMAIL_TEMPLATE_PARAMS: string
     }
 }
